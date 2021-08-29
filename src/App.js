6864e9b2f494c2e0a8997e21548c7e2a4476ae10
Amateratsu888 <vendell.cg@gmail.com>
2021-08-29 05:14:28 +0100

    premier checkpoint react

--- src/App.js
+++ src/App.js
@@ -5,29 +5,48 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div id="login " className=" well well-lg bg-secondary" style={{height:'100vh'}}>
-    <h3 className="text-center text-white pt-5">Login form</h3>
+    <div  className=" well well-lg bg-secondary" style={{height:'100vh'}}>
+    <h3 className="text-center text-white pt-5">Formulaire d'inscription</h3>
     <div className="container">
-        <div id="login-row" className="row justify-content-center align-items-center">
-            <div id="login-column" className="col-md-6">
-                <div id="login-box" className="col-md-12">
-                    <form id="login-form" className="form">
-                        <h3 className="text-center text-info">Login</h3>
+        <div className="row justify-content-center align-items-center">
+            <div  className="col-md-6">
+                <div  className="col-md-12">
+                    <form  className="form">
                         <div className="form-group">
-                            <label for="username" className="text-info">Username:</label><br/>
-                            <input type="text" name="username" id="username" className="form-control"/>
+                            <label for="username" className="text-info">Nom:</label><br/>
+                            <input type="text"  className="form-control"/>
                         </div>
+                        <br/>
+                        <div className="form-group">
+                            <label for="username" className="text-info">Prenom:</label><br/>
+                            <input type="text"  className="form-control"/>
+                        </div>
+                        <br/>
+                        <div className="form-group">
+                            <label for="username" className="text-info">Email:</label><br/>
+                            <input type="mail"  className="form-control"/>
+                        </div>
+                        <br/>
+                        <div className="form-group">
+                            <label for="username" className="text-info">Adresse:</label><br/>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <br/>
+                        <div className="form-group">
+                            <label for="username" className="text-info">Telephone:</label><br/>
+                            <input type="number" className="form-control"/>
+                        </div>
+                        <br/>
                         <div className="form-group">
                             <label for="password" className="text-info">Password:</label><br/>
-                            <input type="text" name="password" id="password" className="form-control"/>
+                            <input type="text"  className="form-control"/>
                         </div>
+                        
                         <div class="form-group">
-                            <label for="remember-me" className="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"/></span></label><br/>
-                            <input type="submit" name="submit" className="btn btn-info btn-md" value="submit"/>
+                    
+                            <input type="submit" className="btn btn-info btn-md" />
                         </div>
-                        <div id="register-link" className="text-right">
-                            <a href="#" className="text-info">Register here</a>
-                        </div>
+                
                     </form>
                 </div>
             </div>
