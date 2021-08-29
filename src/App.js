66e76d740672fa9e9c7051c6c4ebbb0d9171a6a9
Amateratsu888 <vendell.cg@gmail.com>
2021-08-29 04:41:10 +0100

    premier checkpoint react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="login " className=" well well-lg bg-secondary" style={{height:'100vh'}}>
+    <h3 className="text-center text-white pt-5">Login form</h3>
+    <div className="container">
+        <div id="login-row" className="row justify-content-center align-items-center">
+            <div id="login-column" className="col-md-6">
+                <div id="login-box" className="col-md-12">
+                    <form id="login-form" className="form">
+                        <h3 className="text-center text-info">Login</h3>
+                        <div className="form-group">
+                            <label for="username" className="text-info">Username:</label><br/>
+                            <input type="text" name="username" id="username" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label for="password" className="text-info">Password:</label><br/>
+                            <input type="text" name="password" id="password" className="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="remember-me" className="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"/></span></label><br/>
+                            <input type="submit" name="submit" className="btn btn-info btn-md" value="submit"/>
+                        </div>
+                        <div id="register-link" className="text-right">
+                            <a href="#" className="text-info">Register here</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
   );
 }
 
